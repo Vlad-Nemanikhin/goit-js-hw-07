420bc3ca90c4fd138be5a8e70f5a1cc3f5757010
Vlad Nemanikhin <77212359+Vlad-Nemanikhin@users.noreply.github.com>
2021-06-08 16:55:16 +0300

    after task 5

---
+++ js/task-05.js
@@ -0,0 +1,15 @@
+'use strict'
+
+const inputEl = document.querySelector('#name-input');
+const spanEl = document.querySelector('#name-output');
+
+inputEl.addEventListener('input', (e)=>{
+	if (e.currentTarget.value.trim() !== ""){
+		spanEl.textContent = e.currentTarget.value;
+	} else {
+		
+		spanEl.textContent = 'незнакомец';
+		
+	};
+
+})
